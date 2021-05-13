d3f068c6f43f844522f67463c5a91c7afc103fa4
Lola <46529244+lgolin@users.noreply.github.com>
2021-05-13 14:28:34 +0100

    pug file

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const path = require('path');
 // Don't know what is it
 app.use(express.json());
 
+// pug connection
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // Create database connection
 const con = mysql.createConnection({
   host: 'localhost',
@@ -19,4 +23,15 @@ con.connect((err) => {
     return;
   }
   console.log('Connection established');
+});
+
+// Routes
+
+app.get('/', (req, res) => {
+  let sql = 'SELECT * FROM schedule ORDER BY id_user asc';
+  con.query(sql, (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.render('schedule', {schedules: results, days, title: 'Schedules'});
+  });
 });
