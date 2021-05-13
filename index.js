d1eb3786533ddaf2882e6a2c02db6653e964800b
Lola <46529244+lgolin@users.noreply.github.com>
2021-05-13 14:55:43 +0100

    pug schedule layout updated with info

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const path = require('path');
+const days = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
 
 // Don't know what is it
 app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // pug connection
 app.set('view engine', 'pug');
@@ -15,6 +30,7 @@ const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
+  database: 'incode',
 });
 
 con.connect((err) => {
@@ -26,12 +42,41 @@ con.connect((err) => {
 });
 
 // Routes
-
 app.get('/', (req, res) => {
-  let sql = 'SELECT * FROM schedule ORDER BY id_user asc';
+  let sql = 'SELECT * FROM schedule ORDER BY id asc';
   con.query(sql, (err, results) => {
     if (err) throw err;
     console.log(results);
     res.render('schedule', {schedules: results, days, title: 'Schedules'});
   });
+});
+
+app.get('/new', function (req, res) {
+  res.render('addSchedule', {days, title: 'Add New Schedule'});
+});
+
+app.post('/new', (req, res) => {
+  console.log(req.body);
+
+  let username = req.body.username;
+  let day = req.body.day;
+  let start_time = req.body.start;
+  let end_time = req.body.end;
+  con.query(
+    'INSERT INTO schedule(username,day,start_time,end_time) VALUES(?,?,?,?)',
+    [username, day, start_time, end_time],
+    function (err) {
+      if (err) {
+        return console.log(err.message);
+      }
+      console.log('New schedule has been added');
+
+      res.redirect('/');
+    }
+  );
+});
+
+// localhost
+app.listen('3000', () => {
+  console.log('server started on port 3000');
 });
